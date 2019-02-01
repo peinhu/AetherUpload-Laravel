af6e349fc82a906948bf89c23abfd79570c42430
peinhu <hu_yang110@163.com>
2019-02-01 18:08:12 +0800

    fix bugs

--- middleware/AetherUploadCORS.php
+++ middleware/AetherUploadCORS.php
@@ -18,7 +18,7 @@ class AetherUploadCORS
     {
         $response = $next($request);
 
-        $response->header('Access-Control-Allow-Origin', ConfigMapper::get('DISTRIBUTED_DEPLOYMENT_WEB_HOSTS'));//允许的来源域名
+        $response->header('Access-Control-Allow-Origin', ConfigMapper::get('distributed_deployment_web_hosts'));//允许的来源域名
         $response->header('Access-Control-Allow-Headers', 'X-CSRF-TOKEN');//允许的自定义头部参数
         $response->header('Access-Control-Allow-Methods', 'POST, OPTIONS');//允许的请求方法
         $response->header('Access-Control-Allow-Credentials', 'true');//是否允许携带cookie
