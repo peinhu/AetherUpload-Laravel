0990ca621cdd7c01152315b9bd4c28ad7980315b
peinhu <hu_yang110@163.com>
2019-03-21 23:13:23 +0800

    fix compatibility issues

--- src/AetherUploadServiceProvider.php
+++ src/AetherUploadServiceProvider.php
@@ -28,8 +28,8 @@ class AetherUploadServiceProvider extends ServiceProvider
             __DIR__ . '/../assets/spark-md5.min.js'         => public_path('vendor/aetherupload/js/spark-md5.min.js'),
             __DIR__ . '/../uploads/aetherupload_file'       => storage_path('app/aetherupload/file'),
             __DIR__ . '/../uploads/aetherupload_header'     => storage_path('app/aetherupload/_header'),
-            __DIR__ . '/../translations/zh/messages.php'    => resource_path('lang/vendor/aetherupload/zh/messages.php'),
-            __DIR__ . '/../translations/en/messages.php'    => resource_path('lang/vendor/aetherupload/en/messages.php'),
+            __DIR__ . '/../translations/zh/messages.php'    => base_path('resource/lang/vendor/aetherupload/zh/messages.php'),
+            __DIR__ . '/../translations/en/messages.php'    => base_path('resource/lang/vendor/aetherupload/en/messages.php'),
             __DIR__ . '/../middleware/AetherUploadCORS.php' => app_path('Http/Middleware/AetherUploadCORS.php'),
         ], 'aetherupload');
 
