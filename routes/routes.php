02992bf3dc6a3e65adc06ec5885c8bcef713e976
peinhu <hu_yang110@163.com>
2019-03-22 14:11:05 +0800

    fix issues

--- routes/routes.php
+++ routes/routes.php
@@ -17,7 +17,7 @@ Route::group(['middleware' => 'web'], function () {
 
             echo PHP_EOL;
             echo '获得已上传资源的<b>访问链接</b>' . PHP_EOL;
-            echo 'a.(手动)通过请求路由"域名(分布式启用时应当为储存服务器的域名)/配置中自定义的访问路由/"+file1 ';
+            echo 'a.(手动)通过请求路由"域名(分布式启用时应当为储存服务器的域名)/aetherupload/display/"+file1 ';
             echo '<a href="' . (\AetherUpload\ConfigMapper::get('distributed_deployment_enable') ? \AetherUpload\ConfigMapper::get('distributed_deployment_storage_host') : '') . '/aetherupload/display/' . request()->input('file1') . '" target="_blank">访问file1</a> ' . PHP_EOL;
             echo 'b.(自动)通过全局帮助方法{{ aetherupload_display_link(file1)  }} ';
             echo '<a href="' . aetherupload_display_link(request()->input('file1')) . '" target="_blank">访问file1</a>' . PHP_EOL;
@@ -26,7 +26,7 @@ Route::group(['middleware' => 'web'], function () {
 
             echo PHP_EOL;
             echo '获得已上传资源的<b>下载链接</b>' . PHP_EOL;
-            echo 'a.(手动)通过请求路由"域名(分布式启用时应当为储存服务器的域名)/配置中自定义的访问路由/"+file1+"/newname" ';
+            echo 'a.(手动)通过请求路由"域名(分布式启用时应当为储存服务器的域名)/aetherupload/download/"+file1+"/newname" ';
             echo '<a href="' . (\AetherUpload\ConfigMapper::get('distributed_deployment_enable') ? \AetherUpload\ConfigMapper::get('distributed_deployment_storage_host') : '') . '/aetherupload/download/' . request()->input('file1') . '/newname" target="_blank">下载file1</a> ' . PHP_EOL;
             echo 'b.(自动)通过全局帮助方法{{ aetherupload_download_link(file1,newname)  }} ';
             echo '<a href="' . aetherupload_download_link(request()->input('file1'), 'newname') . '" target="_blank">下载file1</a>' . PHP_EOL;
