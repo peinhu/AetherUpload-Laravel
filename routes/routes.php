03c50f14fa8eeafc36050b73197124b4a957bfcb
ran <someone>
2022-07-27 18:11:43 +0800

    apply project route middleware default key

--- routes/routes.php
+++ routes/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['middleware' => 'web'], function () {
+Route::group(['middleware' => \AetherUpload\ConfigMapper::get('middleware_base')], function () {
 
     if ( Config::get('app.env') !== 'production' ) {
 
